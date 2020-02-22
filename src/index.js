5160a70c86f99a78f8e796397129fe72e32684ae
Gui <guilhermehenry60@gmail.com>
2020-02-21 21:30:05 -0300

    chore: add react navigation

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#333',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#FFF',
-    fontSize: 40,
-    fontWeight: '500',
-    textAlign: 'center',
-  },
-});
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>My APP</Text>
-    </View>
-  );
+  return <Routes />;
 }
